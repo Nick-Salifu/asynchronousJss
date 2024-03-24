09c4eedf09ebb939a678c201e45d90832e32c5d7
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-03-24 23:25:16 +0100

    promise

--- script.js
+++ script.js
@@ -19,12 +19,9 @@ const renderCountry = function(data, className = ''){
   </article>
     `
     countriesContainer.insertAdjacentHTML('beforeend', html);
-    // countriesContainer.style.opacity = 1;
+    countriesContainer.style.opacity = 1;
 }
-const renderError = function(msg){
-  countriesContainer.insertAdjacentText('beforeend', msg)
-  // countriesContainer.style.opacity = 1;
-}
+
 
 
 // const getCountryAndNeighbour = function(country){
@@ -68,28 +65,17 @@ const renderError = function(msg){
 // console.log(request);
 
 
-
-const getCountryData = function(country) {
-    fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(response => response.json())
-    .then(data => { renderCountry(data[0])
-
+const getCountryData = function(country){
+  fetch(`https://restcountries.com/v2/name/${country}`)
+  .then(response => response.json())
+  .then(data => {
+    renderCountry(data[0])
     const neighbour = data[0].borders?.[0];
-    // if (!neighbour) return;
-
-    return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
-    })
-    .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour'))
-    .catch(err => {
-      console.error(`${err}`)
-      renderError(`Something went wrong ${err.message}. Try again!`)
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    })
-};
-
-btn.addEventListener('click', function(){
-  getCountryData('portugal')
-})
+    
+   return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+  })
+  .then(response => response.json())
+  .then(data => renderCountry(data, 'neighbour'))
+  
+}
+getCountryData('usa')
